a4c2994d69dd033a8db647ce7b0fa62b862a8f7c
Hugo-18 <hugocifuentescifuentes@gmail.com>
2026-09-21 21:05:40 -0600

    agregar estilos y primera interaccion con js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 // Archivo de JavaScript del proyecto.
 // Se empieza a trabajar en la sesión 4.
 // El código se escribe debajo de este comentario.
+//console.log("el script se esta ejecutando");
+
+const boton = document.querySelector("#ver-mas");
+
+const extra = document.querySelector("#proyectos-extra");
+
+boton.addEventListener("click", function () {
+    extra.classList.toggle("oculto");
+});
